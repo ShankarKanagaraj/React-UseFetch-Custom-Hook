eb3b504783841fb9318a4aab48e58adb77e1ffe9
ShankarKanagaraj <shankar.ss641008@gmail.com>
2024-05-02 19:52:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UseFetchHookTest from './components/use-fetch/test'
+
+function App() {
+
+  return (
+    <>
+    <UseFetchHookTest />
+    </>
+  )
+}
+
+export default App
